d894392d169a0d2c259532420a1b2a3e24e47d46
Carreon-9E-ay2526 <carreon.jo@obmontessori.edu.ph>
2026-04-24 10:06:54 +0800

    added coding

---
+++ main.js
@@ -0,0 +1,7 @@
+function countanimals(){
+    var animals = ["Lion", "Flamingo", "Bear", "Boa Constrictor", "Anchovy", "Beetle", "Chi Chi", "Dusky Shark", "Eastern Phoebe", "Football Fish", "Golden-Crowned Flying Fox", "Horsefly", "Ivy Bee", "Jaguar", "Keagle", "Luna Moth", "Meiolania", "Norway Rat", "Otter", "Patagonian Cavy", "Quail", "Rockfish", "Sardines", "Titanosaur", "Umbrellabrid", "Vampire Carb", "Wax Moth", "Xiaotingia", "Yellowhammer", "Zebra Shark"];
+
+    for (var i = 0; i < animals.length; i++) {
+    window.alert("This zoo contains a " + animals[i] + ".");    
+    }
+}
